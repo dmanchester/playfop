3d8e0da75018e611dec440fdf1749b2dff0bc64d
Daniel Manchester <dpmanchester@gmail.com>
2017-06-02 22:19:00 -0400

    Adapt tests to re-worked PlayFOP API.

--- library/src/test/scala/com/dmanchester/playfop/TestHelpers.scala
+++ library/src/test/scala/com/dmanchester/playfop/TestHelpers.scala
@@ -4,12 +4,14 @@ import java.io.ByteArrayInputStream
 import java.io.StringWriter
 
 import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.pdmodel.PDPage
 import org.apache.pdfbox.util.PDFTextStripper
 
 import scala.xml.Elem
 import scala.xml.Node
 import scala.xml.Text
 import scala.xml.XML
+import scala.collection.JavaConverters._
 
 import play.twirl.api.Xml
 import play.twirl.api.XmlFormat
@@ -18,32 +20,31 @@ import play.twirl.api.XmlFormat
   */
 object TestHelpers {
 
-  /** Wraps text in a simple XSL-FO document, returning it as Twirl XML.
-    *
-    * @param text the text to wrap
-    */
-  def wrapInXslfoDocument(text: String): Xml = wrapInXslfoDocument(Text(text))
-
-  /** Wraps an XML Node in a simple XSL-FO document, returning it as Twirl XML.
+  /** Wraps an XML Node in a simple XSL-FO document, optionally applying a font.
+    * Returns the document as Twirl XML.
     *
     * @param node the Node to wrap
+    * @fontFamily the font to apply (optional)
     */
-  def wrapInXslfoDocument(node: Node): Xml = toTwirlXml(
-    <fo:root xmlns:fo="http://www.w3.org/1999/XSL/Format">
-      <fo:layout-master-set>
-        <fo:simple-page-master master-name="label">
-          <fo:region-body region-name="xsl-region-body"/>
-        </fo:simple-page-master>
-      </fo:layout-master-set>
-      <fo:page-sequence master-reference="label">
-        <fo:flow flow-name="xsl-region-body">
-          <fo:block>{node}</fo:block>
-        </fo:flow>
-      </fo:page-sequence>
-    </fo:root>
-  )
+  def wrapInXslfoDocument(node: Node, fontFamily: Option[String] = None): Xml = {
+    val fontFamilyAsText = fontFamily.map { Text(_) }
+    toTwirlXml(
+      <fo:root xmlns:fo="http://www.w3.org/1999/XSL/Format">
+        <fo:layout-master-set>
+          <fo:simple-page-master master-name="label">
+            <fo:region-body region-name="xsl-region-body"/>
+          </fo:simple-page-master>
+        </fo:layout-master-set>
+        <fo:page-sequence master-reference="label">
+          <fo:flow flow-name="xsl-region-body">
+            <fo:block font-family={fontFamilyAsText}>{node}</fo:block>
+          </fo:flow>
+        </fo:page-sequence>
+      </fo:root>
+    )
+  }
 
-  private def toTwirlXml(xml: Elem): play.twirl.api.Xml = {
+  private def toTwirlXml(xml: Elem): Xml = {
     val stringWriter = new StringWriter()
     XML.write(stringWriter, xml, "utf-8", true /* xmlDecl */, null /* doctype */)
     XmlFormat.raw(stringWriter.toString())
@@ -64,6 +65,16 @@ object TestHelpers {
   def authorFromPDFBytes(pdfBytes: Array[Byte]) = {
     val pdDocument = toPDDocument(pdfBytes)
     pdDocument.getDocumentInformation().getAuthor()
+  }
+
+  def fontsFromPDFBytes(pdfBytes: Array[Byte]) = {
+    val pdDocument = toPDDocument(pdfBytes)
+    val pdPages: java.util.List[PDPage] = pdDocument.getDocumentCatalog().getAllPages().asInstanceOf[java.util.List[PDPage]]
+
+    pdPages.asScala.foldLeft(Set.empty[String]) { case(theSet, pdPage) =>
+      val fonts = pdPage.getResources().getFonts().values().asScala.map { _.getBaseFont() }
+      theSet ++ fonts
+    }
   }
 
   private def toPDDocument(pdfBytes: Array[Byte]) = PDDocument.load(new ByteArrayInputStream(pdfBytes))
--- library/src/test/scala/com/dmanchester/playfop/sapi/PlayFopSpec.scala
+++ library/src/test/scala/com/dmanchester/playfop/sapi/PlayFopSpec.scala
@@ -12,6 +12,7 @@ import scala.xml.XML
 
 import org.apache.fop.apps.FOUserAgent
 import org.apache.fop.apps.Fop
+import org.apache.fop.fo.FOTreeBuilder
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.xmlgraphics.util.MimeConstants
 import org.specs2.mutable.Specification
@@ -19,15 +20,17 @@ import org.specs2.mutable.Specification
 import javax.xml.transform.TransformerFactory
 import javax.xml.transform.sax.SAXResult
 import javax.xml.transform.stream.StreamSource
+
 import play.twirl.api.Xml
 import play.twirl.api.XmlFormat
+
+import scala.collection.JavaConverters._
+import scala.xml.Text
 
 class PlayFopSpec extends Specification {
 
   val PdfText = "Hello there"
   val PdfAuthor = "PlayFopSpec"
-
-  val Xslfo = TestHelpers.wrapInXslfoDocument(PdfText)
 
   val FOUserAgentBlock = { foUserAgent: FOUserAgent =>
     foUserAgent.setAuthor(PdfAuthor)
@@ -36,22 +39,40 @@ class PlayFopSpec extends Specification {
   "process(xslfo, outputFormat)" should {
     "render the XSL-FO in the chosen format" in {
 
-      val pdfBytes = PlayFop.process(Xslfo, MimeConstants.MIME_PDF)
+      val xslfo = TestHelpers.wrapInXslfoDocument(Text(PdfText))
+      val pdfBytes = PlayFop.process(xslfo, MimeConstants.MIME_PDF)
 
       TestHelpers.textFromPDFBytes(pdfBytes) must beEqualTo(PdfText)
     }
   }
 
-  "process(xslfo, outputFormat, autoDetectFontsForPDF, foUserAgentBlock)" should {
-    "render the XSL-FO in the chosen format, auto-detecting fonts and applying the FOUserAgent block" in {
+  "process(xslfo, outputFormat, autoDetectFontsForPDF)" should {
+    "render the XSL-FO with the chosen font" in {
 
-      val pdfBytes = PlayFop.process(Xslfo, MimeConstants.MIME_PDF,
-          autoDetectFontsForPDF = true, foUserAgentBlock = FOUserAgentBlock)
+      val fop = PlayFop.newFop(MimeConstants.MIME_PDF, new ByteArrayOutputStream(), autoDetectFontsForPDF = true)
+
+      // To confirm a specific font is used, we choose one outside the PDF
+      // format's base 14 fonts. (If we were to choose a base font, it could
+      // potentially get used by default, even if process() weren't rendering
+      // the XSL-FO correctly.)
+
+      val fontFamily = chooseFontFamilyOutsideBase14WithSingleWordName(getFontFamilies(fop))
+      val xslfo = TestHelpers.wrapInXslfoDocument(Text(PdfText), Some(fontFamily))
+
+      val pdfBytes = PlayFop.process(xslfo, MimeConstants.MIME_PDF, autoDetectFontsForPDF = true)
+
+      TestHelpers.fontsFromPDFBytes(pdfBytes) must contain(fontFamily)
+    }
+  }
+
+  "process(xslfo, outputFormat, foUserAgentBlock)" should {
+    "render the XSL-FO in the chosen format, applying the FOUserAgent block" in {
+
+      val xslfo = TestHelpers.wrapInXslfoDocument(Text(PdfText))
+      val pdfBytes = PlayFop.process(xslfo, MimeConstants.MIME_PDF, foUserAgentBlock = FOUserAgentBlock)
 
       TestHelpers.textFromPDFBytes(pdfBytes) must beEqualTo(PdfText)
       TestHelpers.authorFromPDFBytes(pdfBytes) must beEqualTo(PdfAuthor)
-
-      // TODO How to validate that autoDetectFontsForPDF was consulted?
     }
   }
 
@@ -60,25 +81,40 @@ class PlayFopSpec extends Specification {
 
       val output = new ByteArrayOutputStream()
       val fop = PlayFop.newFop(MimeConstants.MIME_PDF, output)
-      process(Xslfo, fop)
+      val xslfo = TestHelpers.wrapInXslfoDocument(Text(PdfText))
+
+      process(xslfo, fop)
 
       TestHelpers.textFromPDFBytes(output.toByteArray()) must beEqualTo(PdfText)
     }
   }
 
-  "newFop(outputFormat, fopConfig, foUserAgentBlock, output)" should {
-    "obtain an Fop for the output format, auto-detecting fonts and applying the FOUserAgent block" in {
+  "newFop(outputFormat, fopConfig, autoDetectFontsForPDF)" should {
+    "obtain an Fop that respects the font choice" in {
 
       val output = new ByteArrayOutputStream()
-      val fop = PlayFop.newFop(MimeConstants.MIME_PDF, output,
-          autoDetectFontsForPDF = true, foUserAgentBlock = FOUserAgentBlock)
-      process(Xslfo, fop)
+      val fop = PlayFop.newFop(MimeConstants.MIME_PDF, output, autoDetectFontsForPDF = true)
+      val fontFamily = chooseFontFamilyOutsideBase14WithSingleWordName(getFontFamilies(fop))
+      val xslfo = TestHelpers.wrapInXslfoDocument(Text(PdfText), Some(fontFamily))
+
+      process(xslfo, fop)
+
+      TestHelpers.fontsFromPDFBytes(output.toByteArray()) must contain(fontFamily)
+    }
+  }
+
+  "newFop(outputFormat, output, foUserAgentBlock)" should {
+    "obtain an Fop for the output format, applying the FOUserAgent block" in {
+
+      val output = new ByteArrayOutputStream()
+      val fop = PlayFop.newFop(MimeConstants.MIME_PDF, output, foUserAgentBlock = FOUserAgentBlock)
+      val xslfo = TestHelpers.wrapInXslfoDocument(Text(PdfText))
+
+      process(xslfo, fop)
 
       val outputAsByteArray = output.toByteArray()
       TestHelpers.textFromPDFBytes(outputAsByteArray) must beEqualTo(PdfText)
       TestHelpers.authorFromPDFBytes(outputAsByteArray) must beEqualTo(PdfAuthor)
-
-      // TODO How to validate that autoDetectFontsForPDF was consulted?
     }
   }
 
@@ -98,5 +134,27 @@ class PlayFopSpec extends Specification {
     val result = new SAXResult(fop.getDefaultHandler())
 
     transformer.transform(source, result)
+  }
+
+  private def getFontFamilies(fop: Fop) = {
+
+    val fop = PlayFop.newFop(MimeConstants.MIME_PDF, new ByteArrayOutputStream(), autoDetectFontsForPDF = true)
+
+    val fontInfo = fop.getDefaultHandler().asInstanceOf[FOTreeBuilder].getEventHandler().getFontInfo()
+
+    val typefaces = fontInfo.getFonts().values().asScala
+
+    typefaces.map(_.getFullName()).toSeq
+  }
+
+  private def chooseFontFamilyOutsideBase14WithSingleWordName(fontFamilies: Iterable[String]) = {
+
+    fontFamilies.find { fontFamily =>
+      "Times|Courier|Helvetica|Symbol|Zapf".r.findFirstIn(fontFamily).isEmpty &&
+        "^\\w*$".r.findFirstIn(fontFamily).isDefined
+    }.get
+    // It would be highly unusual for there to be no font families that meet
+    // both criteria. So, the previous line throwing an Exception in the "None"
+    // case is actually desirable.
   }
 }
