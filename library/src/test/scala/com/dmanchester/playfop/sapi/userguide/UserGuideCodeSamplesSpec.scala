ddcda2d8d576e944e08b5428e3787f3fc76066ae
Daniel Manchester <dpmanchester@gmail.com>
2018-02-24 23:06:48 -0500

    Discuss additional newline preservers in user guide
    Also, switch newline-preserver sample code to a Scala XML template.

--- library/src/test/scala/com/dmanchester/playfop/sapi/userguide/UserGuideCodeSamplesSpec.scala
+++ library/src/test/scala/com/dmanchester/playfop/sapi/userguide/UserGuideCodeSamplesSpec.scala
@@ -51,4 +51,42 @@ val pdf: Array[Byte] = playFop.processStringXml(
       // used. The main tests verify that behavior.
     }
   }
+
+  "This code sample" should {
+    "render the XSL-FO as PDF, preserving whitespace" in new playFopBlock {
+
+      val text = "H e l l o\n\n w o r l d"
+
+// BEGIN Formatters sample
+// IMPORTANT: If following line is changed, UserGuide.scalatex must be changed
+// in kind!
+import com.dmanchester.playfop.api.Formatters
+
+val formattedText = Formatters.preserveNewlinesForScalaXml(
+  Formatters.preserveSpaces(text)
+)
+
+val xslfo =
+<fo:root xmlns:fo="http://www.w3.org/1999/XSL/Format">
+  <fo:layout-master-set>
+    <fo:simple-page-master master-name="label">
+      <fo:region-body region-name="xsl-region-body"/>
+    </fo:simple-page-master>
+  </fo:layout-master-set>
+  <fo:page-sequence master-reference="label">
+    <fo:flow flow-name="xsl-region-body">
+      <fo:block font-family="Courier" font-size="24pt">{formattedText}</fo:block>
+    </fo:flow>
+  </fo:page-sequence>
+</fo:root>
+// END Formatters sample
+
+      val pdf: Array[Byte] = playFop.processScalaXml(
+        xslfo,
+        MimeConstants.MIME_PDF
+      )
+
+      TestHelpers.textFromPDFBytes(pdf) must beEqualTo("H e l l o\n \n w o r l d")  // second line (which is blank) renders with an extraneous space, which is acceptable
+    }
+  }
 }
